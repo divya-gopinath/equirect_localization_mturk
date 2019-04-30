bd4b41e266e57d76dd0cbefff79a8f804a7102af
Divya Gopinath <divya@LAPTOP-27SB9AS3.mit.edu>
2019-04-30 01:04:39 -0400

    Modifications to post and retrieve hits

--- post_hits.py
+++ post_hits.py
@@ -8,7 +8,7 @@ print(question)
 
 mturk = boto3.client('mturk', endpoint_url = MTURK_SANDBOX)
 new_hit = mturk.create_hit(
-    Title = 'TRY 3: Can you localize sounds in this video?',
+    Title = 'TRY 4: Can you localize sounds in this video?',
     Description = 'Using 3-D camera feed and two-channel audio, localize the sounds in this video.',
     Keywords = 'video, audio, labelling',
     Reward = '0.0',
--- retrieve_hits.py
+++ retrieve_hits.py
@@ -3,7 +3,7 @@ import boto3
 MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
 mturk = boto3.client('mturk', endpoint_url = MTURK_SANDBOX)
 # Use the hit_id previously created
-hit_id = '3DIIW4IV8PKK9PK98QH1WQ8TVP54I0'
+hit_id = '3G3AJKPCXLH6ZPZEC834IE2HL834Y6'
 # We are only publishing this task to one Worker
 # So we will get back an array with one item if it has been completed
 worker_results = mturk.list_assignments_for_hit(HITId=hit_id, AssignmentStatuses=['Submitted'])
