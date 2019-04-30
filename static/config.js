4b52788de68a1f860e0148396aecef229f74c927
Divya Gopinath <divya@LAPTOP-27SB9AS3.mit.edu>
2019-04-30 00:32:58 -0400

    Possible bug with endpoint?

--- static/config.js
+++ static/config.js
@@ -8,7 +8,7 @@ const CSS_GLOW = "0px 0px 30px 20px #ffff";
 const CSS_TEXT_GLOW = "2px 2px 5px ";
 
 const HIGHLIGHT_MODE_TEXT = "You are in highlight mode. Update the point in question and press the down arrow key. You may add new sources at the end.";
-const SUBMIT_ENDPOINT = "https://workersandbox.mturk.com/mturk/externalSubmit";
+const SUBMIT_ENDPOINT = "https://workersandbox.mturk.com/mturk/externalSubmit/";
 
 var randomColor = function() {
   return '#'+Math.floor(Math.random()*16777215).toString(16);
