770ca6840d11fd563aff8ba434218bcd49f8d664
Divya Gopinath <divya@LAPTOP-27SB9AS3.mit.edu>
2019-04-29 23:25:29 -0400

    Add ability to get assignmentId

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    print(request.args)
+    print(request.form)
     return render_template("intro.html")
 
 @app.route("/continue", methods=['GET', 'POST'])
