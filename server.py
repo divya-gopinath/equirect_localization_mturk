4228ae631a86445f98521714415fd67d32188021
Divya Gopinath <divya@LAPTOP-27SB9AS3.mit.edu>
2019-04-29 21:46:01 -0400

    Basic question functionality

--- server.py
+++ server.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return render_template("intro.html")
+
+@app.route("/continue")
+def continue():
     return render_template("index.html")
 
 if __name__ == "__main__":
